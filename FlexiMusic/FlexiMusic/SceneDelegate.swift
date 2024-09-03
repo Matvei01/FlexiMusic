d49cf01fc5b3087363180095fc1f7093781bfae6
Matvei <khlestov.matvei@mail.ru>
2024-09-03 18:26:00 +0500

    Created base project, removed storyboard references

---
+++ FlexiMusic/FlexiMusic/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  FlexiMusic
+//
+//  Created by Matvei Khlestov on 03.09.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+}
+
